5ab7ee0f6526fa32844aac9877f5c0f51ef98825
Joshua Rodriguez <ewized@gmail.com>
2016-04-02 11:08:44 -0700

    Base layout of the program

--- src/percentile/Main.java
+++ src/percentile/Main.java
@@ -1,4 +1,75 @@
 package percentile;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+/** This program will read the file located in the current working director , or from arg -Dfile_name */
 public class Main {
+    private static final String FILE_NAME = System.getProperty("file_name", "median.txt");
+
+
+    public static void main(String[] args) {
+        List<String> input = readInput();
+        input.forEach(line -> {
+            int[] elemetns = parseInput(line);
+            outputPercentile(elemetns);
+        });
+    }
+
+    /** Out put the percentile of the data set */
+    public static void outputPercentile(int[] integers) {
+        integers = Sorting.mergeSort(integers);
+        double median = IntStream.of(integers).sum() / integers.length;
+        double percentile = (median * 100) / integers.length;
+
+        System.out.println(median + " - " + percentile);
+    }
+
+    /** Print the array for debugging purposes */
+    public static void printArray(int[] array) {
+        for (int item : array) {
+            System.out.print(item + " ");
+        }
+        System.out.println();
+    }
+
+    /** Read the inputs from the input file */
+    private static List<String> readInput() {
+        File file = new File(FILE_NAME);
+        List<String> lines = new ArrayList<>();
+
+        try (InputStreamReader reader = new InputStreamReader(new FileInputStream(file))) {
+            Scanner in = new Scanner(reader);
+            while (in.hasNextLine()) {
+                String line = in.nextLine();
+                if (line == null || line.isEmpty()) continue;
+                lines.add(in.nextLine());
+            }
+        } catch (IOException error) {
+            System.out.println(String.format("Could not find a file name by %s", FILE_NAME));
+        }
+
+        return lines;
+    }
+
+    /** Gather the list of inputs from a string of numbers separated by a comma */
+    private static int[] parseInput(String line) {
+        String[] parts = line.split(",");
+        int pos = 0;
+        int[] inputs = new int[parts.length];
+
+        for (String part : parts) {
+            try {
+                // Faster to check than filling stack trace
+                if (part == null || part.isEmpty()) continue;
+                inputs[pos++] = Integer.valueOf(part);
+            } catch (NumberFormatException | IllegalStateException error) {} // do not need anything
+        }
+
+        return Arrays.copyOf(inputs, pos + 1);
+    }
 }
--- src/percentile/Sorting.java
+++ src/percentile/Sorting.java
@@ -1,4 +1,61 @@
 package percentile;
 
-public class Sorting {
+import java.util.Arrays;
+
+/** Contains sorting algorithms for sorting */
+public final class Sorting {
+    private Sorting() {} // Util class
+
+    /** A bubble sort to sort integers */
+    public static int[] bubbleSort(int[] array) {
+        boolean sorted;
+        do {
+            sorted = true;
+            for (int i = 1; i < array.length; i++) {
+                int a = array[i - 1];
+                int b = array[i];
+
+                if (a > b) {
+                    array[i - 1] = b;
+                    array[i] = a;
+                    sorted = false;
+                }
+            }
+        } while (!sorted);
+
+        return array;
+    }
+
+    /** Merge sort an array of integers */
+    public static int[] mergeSort(int[] array) {
+        if (array.length <= 1) {
+            return array;
+        } else if (array.length == 2) { // Do the sort here for better performance
+            return (array[1] < array[0]) ? new int[] {array[1], array[0]} : array;
+        }
+
+        // Split the data set in to two parts
+        int middle = array.length / 2;
+        int[] left = Arrays.copyOfRange(array, 0, middle);
+        int[] right = Arrays.copyOfRange(array, middle, array.length);
+
+        return mergeSort(mergeSort(left), mergeSort(right));
+    }
+
+    /** Internal use for the mergeSort */
+    private static int[] mergeSort(int[] left, int[] right) {
+        int[] merged = new int[left.length + right.length];
+        int l = 0, r = 0; // l = left index | r = right index
+
+        // Sort the data that we have
+        while (l < left.length && r < right.length) {
+            merged[l + r] = (left[l] > right[r]) ? right[r++] : left[l++];
+        }
+
+        // Copy the rest of the data if there is
+        System.arraycopy(left, l, merged, l + r, left.length - l);
+        System.arraycopy(right, r, merged, l + r, right.length - r);
+
+        return merged;
+    }
 }
